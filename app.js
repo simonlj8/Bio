be063f2a70e4ef0a0d12f1ebd03b493400c408ca
Simon Ljungström <simonlj8@gmail.com>
2022-01-22 13:37:39 +0100

    small fixes on movie page

--- app.js
+++ app.js
@@ -10,27 +10,30 @@ app.engine("handlebars", engine({
     markdown: md => marked(md),
   },
 }));
+
 app.set("view engine", "handlebars");
-
 app.set("views", "./views");
-
 app.get("/", async (req, res) => {   
   res.render("index");
 });
 
-app.get("/aboutus", (req, res) => {
+app.get("/aboutus", async (req, res) => {
   res.render("./partials/aboutus")
 });
-
-app.get("/movie", async (req, res) => {
+/*
+app.get("/movie", async (rew, res) => {
+  res.render("./partials/movie")
+});
+*/
+app.get("/movies", async (req, res) => {
   const movies = await loadAllMovies()
-  res.render("./partials/movie", { movies });
+  res.render("./partials/movies", { movies });
 });
 
 app.get("/movies/:movieId", async (req, res) => {
   const movie = await loadMovie(req.params.movieId);
   if (movie) {
-    res.render("./partials/movie", { movie });
+    res.render("./partials/movies", { movie });
   } else {
     res.status(404).render("./partials/404");
   }
